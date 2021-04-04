1b43d88b913c04b86efb41f97f5b4422b96f21a5
Christian Doucette <cdoucette@uchicago.edu>
2021-04-04 02:55:57 -0500

    game done, except need to calculate scores and integrate bot

--- app.py
+++ app.py
@@ -12,18 +12,21 @@ socketio.init_app(app, cors_allowed_origins="*")
 # global variable set containing all connections currently connected through sockets
 # I know global variables like this are bad practice, but its a hackathon, not a best-practiceathon
 all_connections = set()
+name_encoder    = dict()
+votes           = dict()
 
 
 
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
+    json['user_name'] = name_encoder[request.sid]
     print('message sent: ' + str(json))
     socketio.emit('my response', json)
 
 
 
-@socketio.on('connect')
+@socketio.on('new_connect')
 def on_connect(methods=['GET', 'POST']):
     print('New user connected!')
 
@@ -45,54 +48,46 @@ def on_disconnect():
     print(f"all_connections: {all_connections}")
 
 
+@socketio.on('record vote')
+def record_vote(vote):
+    print('Going to record vote, using global variable!')
+    print(f'{request.sid} voted for {vote}')
+
+
+    global votes
+    votes[request.sid] = vote
+
 
 
 @socketio.on('game start')
 def on_game_start(methods=['GET', 'POST']):
     global all_connections
+    global name_encoder
+
     all_players = list(all_connections.copy()) + ['bot']
     random_player_ordering = random.sample(all_players, k=len(all_players))
-    random_player_names    = {player_name: f'Player {index}' for index, player_name in enumerate(random_player_ordering)}
+    name_encoder           = {player_sid: f'Player {index}' for index, player_sid in enumerate(random_player_ordering)}
 
     print('received game start event')
-    print(f'Players: {random_player_names}')
+    print(f'Players: {name_encoder}')
 
     # creates model here
-    socketio.emit('game started', random_player_names)
+    socketio.emit('game started')
     time.sleep(10)
-    socketio.emit('voting started')
+    socketio.emit('voting started', len(all_connections) + 1)
     time.sleep(10)
-    socketio.emit('voting ended')
+    socketio.emit('tallying votes')
+    time.sleep(5)
+    print(votes)
+    # calculate scores here
+    #name_decoder = {val: key for key: val in name_encoder.items()}
+    scores = {conn: 0 for conn in all_connections}
+    socketio.emit('voting ended', scores)
     time.sleep(5)
     socketio.emit('back to start')
 
 
 
-# won't allow players to end game, does it based off time limit
-#@socketio.on('game end')
-#def on_game_end(methods=['GET', 'POST']):
-#    print('received game end event')
-#    socketio.emit('game end')
-
-"""
-@socketio.on('join')
-def on_join(data):
-    print('JOIN REQUEST RECIEVED!')
-    username = data['username']
-    room = data['room']
-    print(f'{username} has entered {room}')
-
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
-
-
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    send(username + ' has left the room.', room=room)
-"""
 
 
 @app.route('/')
