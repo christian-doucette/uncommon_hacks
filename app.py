8c6deaed761aa335a892f7ea4c7b08968d17981d
Christian Doucette <cdoucette@uchicago.edu>
2021-04-04 01:33:34 -0500

    now stores connections to socket as global variable

--- app.py
+++ app.py
@@ -1,32 +1,67 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO #, join_room, leave_room, send
+
 import time
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecret'
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
 
+# global variable set containing all connections currently connected through sockets
+# I know global variables like this are bad practice, but its a hackathon, not a best-practiceathon
+all_connections = set()
 
 
-
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
 
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+    print('message sent: ' + str(json))
+    socketio.emit('my response', json)
+
+
+
+@socketio.on('connect')
+def on_connect(methods=['GET', 'POST']):
+    print('New user connected!')
+
+    # increments num players
+    global all_connections
+    all_connections.add(request.sid)
+    print(f"connections: {all_connections}")
+
+
+
+@socketio.on('disconnect')
+def on_disconnect():
+    print('Client disconnected!')
+
+    # decrements num players
+
+    global all_connections
+    all_connections.discard(request.sid)
+    print(f"all_connections: {all_connections}")
+
+
 
 
 @socketio.on('game start')
 def on_game_start(methods=['GET', 'POST']):
+    global all_connections
+    all_players = list(all_connections.copy()) + ['bot']
+    random_player_ordering = random.sample(all_players, k=len(all_players))
+    random_player_names    = {player_name: f'Player {index}' for index, player_name in enumerate(random_player_ordering)}
+
     print('received game start event')
-    socketio.emit('game started')
-    time.sleep(5)
+    print(f'Players: {random_player_names}')
+
+    # creates model here
+    socketio.emit('game started', random_player_names)
+    time.sleep(10)
     socketio.emit('voting started')
-    time.sleep(5)
+    time.sleep(10)
     socketio.emit('voting ended')
     time.sleep(5)
     socketio.emit('back to start')
